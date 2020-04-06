be45d921380f3181b47c3e51cb9b5ce5f9b4291c
MuzLin <muzlingm@gmail.com>
2020-04-06 16:33:48 +0800

    安装React 和 ReactDom

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,8 @@
-console.log('hi hhh')
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+console.log(React)
+console.log(ReactDOM)
 let div = document.createElement('div')
 div.innerText = '111'
 document.body.appendChild(div)

79a20b347062439f9c81d2ac22909c99887330db
MuzLin <muzlingm@gmail.com>
2020-04-06 15:36:40 +0800

    将index.tsx 打包成 index.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// 引入path模块
+const path = require('path')
+module.exports = {
+  mode: 'production', // 模式 开发、发布等
+  // 入口
+  entry: {
+    index: './lib/index.tsx', // 入口的名字 index 值为具体路径
+  },
+  // 输出
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'), // 输出路径 因为操作系统不一致 所有需要path来处理
+    library: 'Muzlin', // 库的名字
+    libraryTarget: 'umd' // 输出格式 (umd/commonjs/amd)
+  },
+  // 配置TSX(等)的解释方式
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // 根据正则匹配tsx/ts文件
+        loader: 'awesome-typescript-loader' // 使用的loader
+      }
+    ]
+  }
+}
